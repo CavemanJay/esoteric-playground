102417509116199afc632dd41e26a34940592708
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2023-02-16 18:32:03 -0500

    Add benchmarks

---
+++ brainfuck/benches/interpreters.rs
@@ -0,0 +1,28 @@
+use std::{env::current_dir, fs, path::PathBuf, str::FromStr};
+
+use brainfuck::{interpret, interpret_with_wrapping};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+fn bench_interpreters(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Interpreters");
+    for prog_file in ["hello_world.bf", "hello_world2.bf"] {
+        let prog = fs::read_to_string(
+            PathBuf::from(current_dir().unwrap())
+                .join("data")
+                .join(prog_file),
+        )
+        .expect("Failed to read input file");
+        group.bench_with_input(BenchmarkId::new("basic", prog_file), &prog, |b, program| {
+            b.iter(|| interpret(program))
+        });
+        group.bench_with_input(
+            BenchmarkId::new("wrapping", prog_file),
+            &prog,
+            |b, program| b.iter(|| interpret_with_wrapping(program)),
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_interpreters);
+criterion_main!(benches);
